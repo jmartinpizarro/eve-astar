6ad534ea738b9fb9c5642bb5fa2fecec65266c0d
Javier Martin <jmartinpizarro04@gmail.com>
2025-03-02 12:14:46 +0100

    feat: add operator for comparing systems

--- main.h
+++ main.h
@@ -10,8 +10,12 @@ using namespace std;
 struct System{
     string system_name;
     float system_security;
-    std::vector<unordered_map<string, System>> adjacent_systems;
-    std::vector<unordered_map<System, int>> distance_to_adj_system;
+    std::vector<unordered_map<string, System*>> adjacent_systems;
+    std::vector<unordered_map<System, double>> distance_to_adj_system;
+
+    bool operator==(const System& other) const {
+        return system_name == other.system_name && system_security == other.system_security;
+    }
 };
 
 struct State{
