23c9f3b3fa26ef4029bd2799150469a676225065
Javier Martin <jmartinpizarro04@gmail.com>
2025-03-02 12:29:09 +0100

    feat: hashmap for systems

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <unordered_map>
 
-int main(int argc, char* argv[]){
-    std::cout << "Hello EVE-Star";
-    return 1;
+#include "main.h"
+
+int main(int argc, char* argv[]) {
+    System s1 = {"Alpha", 1.0, {}, {}};
+    System s2 = {"Tango", 2.0, {}, {}};
+    System s3 = {"Gamma", 3.0, {}, {}};
+
+    s1.adjacent_systems["Tango"] = &s2;
+    s1.adjacent_systems["Gamma"] = &s3;
+
+    s1.distance_to_adj_system[&s2] = 1.5;
+    s1.distance_to_adj_system[&s3] = 2.3;
+
+    std::cout << "System " << s1.system_name << " is connected to:\n";
+    for (const auto& [name, sys] : s1.adjacent_systems) { // cover all the .keys()
+        std::cout << "- " << name << " with distance " << s1.distance_to_adj_system[sys] << "\n";
+    }
+
+    return 0;
 }
--- main.h
+++ main.h
@@ -10,16 +10,26 @@ using namespace std;
 struct System{
     string system_name;
     float system_security;
-    std::vector<unordered_map<string, System*>> adjacent_systems;
-    std::vector<unordered_map<System, double>> distance_to_adj_system;
+    std::unordered_map<string, System*> adjacent_systems;
+    std::unordered_map<System*, double> distance_to_adj_system;
 
     bool operator==(const System& other) const {
         return system_name == other.system_name && system_security == other.system_security;
     }
 };
 
+// define a hashmap for System being a key
+namespace std{
+    template <>
+    struct hash<System> {
+        size_t operator()(const System& s) const {
+            return hash<string>()(s.system_name);
+        }
+    };
+}
+
 struct State{
     System currentSystem;
 };
 
-#endif // MAIN_H
+#endif
