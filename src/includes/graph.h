310456707b36e3909c786108b7cbef94f19051b0
Javier Martín <jmartinpizarro04@gmail.com>
2025-03-02 19:44:27 +0100

    feat: create graph struct

---
+++ src/includes/graph.h
@@ -0,0 +1,14 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "system.h"
+
+struct Graph{
+    System* nodes[];
+};
+
+/*
+* TODO: migrate to a class, create methods for inserting nodes
+*/
+
+#endif
