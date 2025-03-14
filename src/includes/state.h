9b0553805dc9e79da69cf73c7a83cf414c951fa7
JAVIER MARTIN PIZARRO <jmartinpizarro04@gmail.com>
2025-03-14 11:20:06 +0100

    feat: add operator == for compairing states

--- src/includes/state.h
+++ src/includes/state.h
@@ -6,6 +6,16 @@
 class State{
     public:
         System* currentSystem;
+
+        // constructor by default is a nullptr
+        State(System* system = nullptr) : currentSystem(system) {}
+
+        bool operator==(const State& other) const {
+            if (!currentSystem || !other.currentSystem) {
+                return currentSystem == other.currentSystem; // both are nullptr
+            }
+            return currentSystem->get_name() == other.currentSystem->get_name();
+        }
 };
 
 // hash for each state
