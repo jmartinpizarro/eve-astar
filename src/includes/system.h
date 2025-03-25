b8bb8db3599849532c64658e30d23aef48263538
JAVIER MARTIN PIZARRO <jmartinpizarro04@gmail.com>
2025-03-25 09:02:31 +0100

    feat: docs of the system header

--- src/includes/system.h
+++ src/includes/system.h
@@ -8,13 +8,16 @@
 #include <stdexcept>
 
 using namespace std;
-
+/**
+ * @class System
+ * @brief Represents a System entity
+ */
 class System {
     private:
-        string system_name;
-        float system_security;
-        unordered_map<string, System*> adjacent_systems;
-        unordered_map<System*, double> distance_to_adj_system;
+        string system_name; // system name
+        float system_security; // system security status
+        unordered_map<string, System*> adjacent_systems; // adjacent systems 
+        unordered_map<System*, double> distance_to_adj_system; // distance to travel (AU) to adjacent systems
     
     public:
         // constructor
@@ -28,48 +31,89 @@ class System {
             return system_name == other.system_name;
         }
 
+        /**
+         * @brief sets the name of the system
+         * @param name the future name of the system
+         */
         int set_name(string name){
             system_name = name;
             return 0;
         }
         
+        /**
+         * @brief gets the system name (private attribute)
+         * @return system_name 
+         */
         string get_name() const {
             return system_name;
         }
 
-        double get_security() const {
-            return system_security;
-        }
-
-        unordered_map<string, System*> get_adjacent_systems() const {
-            return adjacent_systems;
-        }
-
-        unordered_map<System*, double> get_adjacent_systems_distance() const {
-            return distance_to_adj_system;
-        }
-
-        double get_adjacent_system_distance(System* sys) {
-            return distance_to_adj_system[sys];
-        }
-
-        int add_adjacent_system(string systemName, System* system) {
-            adjacent_systems[systemName] = system;
-            return 0;
-        }
-
-        int add_adjacent_system_distance(System* system, double distance) {
-            distance_to_adj_system[system] = distance;
-            return 0;
-        }
-
-        // for controlling CONCORD Security values
+        /**
+         * @brief sets the system security status (private attribute)
+         * @param sec security value for that system
+         * @return void 
+         */
         void set_security(double sec){
             if (sec > 1.0 || sec < -1.0){
                 throw out_of_range("Security status must be an float -1.0 < sec < 1.0");
             } else {
                 system_security = sec;
             }
+        }
+
+        /**
+         * @brief gets the system security status (private attribute)
+         * @return system_security 
+         */
+        double get_security() const {
+            return system_security;
+        }
+
+        /**
+         * @brief gets the adjacent systems atribute (private attribute)
+         * @return adjacent_system (u_map) 
+         */
+        unordered_map<string, System*> get_adjacent_systems() const {
+            return adjacent_systems;
+        }
+
+        /**
+         * @brief gets the distance of the adjacent systems atribute (private attribute)
+         * @return distance_to_adj_system (u_map)
+         */
+        unordered_map<System*, double> get_adjacent_systems_distance() const {
+            return distance_to_adj_system;
+        }
+
+        /**
+         * @brief gets the adjacent distance to an adjacent System (private attribute)
+         * @param sys the adjacent system to research for
+         * @return distance (double)
+         */
+        double get_adjacent_system_distance(System* sys) {
+            return distance_to_adj_system[sys];
+        }
+
+        /**
+         * @brief add an adjacent system to a System (private attribute)
+         * @param systemName the adjacent system name
+         * @param system a pointer to system 
+         * @return 0
+         */
+        int add_adjacent_system(string systemName, System* system) {
+            adjacent_systems[systemName] = system;
+            return 0;
+        }
+
+        /**
+         * @brief add an adjacent system distance to a System (private attribute)
+         * @param system a pointer to system 
+         * @param distance distance to add to that system
+         * @return 0
+         */
+        int add_adjacent_system_distance(System* system, double distance) {
+            distance_to_adj_system[system] = distance;
+            return 0;
         }
 };
 
